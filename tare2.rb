ed31d49740eaa1245fee3eefa816dc4f8626e537
Uriel Alejandro Arreola Larios <L20290831@cdguzman.tecnm.mx>
2024-09-05 22:38:30 +0000

    Tarea2

---
+++ tare2.rb
@@ -0,0 +1,23 @@
+class ContadorPalabras
+    attr_accessor :texto
+  
+    def initialize(texto)
+      @texto = texto
+    end
+  
+    def contar_palabras
+
+      palabras = @texto.split(' ')
+      contador = Hash.new(0)
+      palabras.each do |palabra|
+        contador[palabra.downcase] += 1
+      end
+      contador.each do |palabra, cuenta|
+        puts "#{palabra} #{cuenta}"
+      end
+    end
+  end
+ 
+  texto = "casa carro carro bicicleta"
+  contador = ContadorPalabras.new(texto)
+  contador.contar_palabras
